5325a749234a068d87fa7acb06c100feb1101cd7
Siarra Price <siarraprice@ufl.edu>
2019-04-28 17:27:41 -0500

    added js file

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+
+var members = ['Dave Grohl',
+              'Nate Mendel',
+              'Pat Smear',
+              'Taylor Hawkins',
+              'Rami Jaffee',
+              'Chris Shiflett'];
+
+var tour = {
+    Chicago: July 29-30,
+    Seattle: September 1,
+    Edmonton: September 4
+  }
+
+while (x=x) {
+  msg = "times running through the loop x"
+}
